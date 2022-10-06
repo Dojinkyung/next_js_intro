057283dd398749599cfe696103bb4a8c77798dca
Dojinkyung <63532503+Dojinkyung@users.noreply.github.com>
2022-10-06 19:32:41 +0900

    feat:영화 api불러오기

---
+++ pages/index.js
@@ -0,0 +1,39 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+import Seo from "../components/Seo";
+
+export default function Home({ results }) {
+  const router = useRouter();
+  const onClick = (id, title) => {
+    router.push(`/movies/${title}/${id}`);
+  };
+  return (
+    <div className="container">
+      <Seo title="Home" />
+      {results?.map((movie) => (
+        <div
+          onClick={() => onClick(movie.id, movie.original_title)}
+          className="movie"
+          key={movie.id}>
+          <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
+          <h4>
+            <Link href={`/movies/${movie.original_title}/${movie.id}`}>
+              <a>{movie.original_title}</a>
+            </Link>
+          </h4>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export async function getServerSideProps() {
+  const { results } = await (
+    await fetch(`http://localhost:3000/api/movies`)
+  ).json();
+  return {
+    props: {
+      results,
+    },
+  };
+}
