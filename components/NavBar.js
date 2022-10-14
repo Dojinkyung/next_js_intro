1215ebb5851b4c08f2f01977453553dae12b955f
Dojinkyung <63532503+Dojinkyung@users.noreply.github.com>
2022-10-14 19:50:13 +0900

    feat: 검색창 css

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,15 +1,30 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
 import styles from "../styles/navBar.module.css";
+import { useMemo, useState } from "react";
+import debounce from "debounce";
 
 export default function NavBar() {
   const router = useRouter();
+  const [searchBar, setSearchBar] = useState(false);
+  const handleSearchbar = () => {
+    setSearchBar((prev) => !prev);
+  };
+  const onSubmit = (event) => {
+    event.preventDefault();
+  };
+  const debouncedResults = useMemo(() => {
+    const handleChange = (event) => {
+      event.target.value.trim();
+    };
+    return debounce(handleChange, 800);
+  }, []);
   return (
     <nav className={styles.nav}>
-      <Link href="/">
-        <img src="../video-movie-svgrepo-com.svg" />
-      </Link>
-      <div>
+      <div className={styles.links}>
+        <Link href="/">
+          <img src="../video-movie-svgrepo-com.svg" />
+        </Link>
         <Link href="/">
           <a className={router.pathname === "/" ? `${styles.active}` : ``}>
             Home
@@ -24,6 +39,29 @@ export default function NavBar() {
           </a>
         </Link>
       </div>
+
+      <form
+        onSubmit={onSubmit}
+        className={!searchBar ? `${styles.none}` : `${styles.form}`}>
+        <div>
+          <input
+            className={styles.input}
+            type="text"
+            placeholder="Search Movie."
+            onChange={debouncedResults}
+          />
+          <button className={styles.button} type="submit">
+            검색
+          </button>
+        </div>
+      </form>
+      <button
+        className={
+          searchBar ? `${styles.rotate}` : `${styles.searchbardownBTN}`
+        }
+        onClick={handleSearchbar}>
+        <img src="../arrow-down-svgrepo-com.svg" />
+      </button>
     </nav>
   );
 }
