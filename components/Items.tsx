912beab6d41ef9d380eedee5e94f3c695e8d4436
Dojinkyung <63532503+Dojinkyung@users.noreply.github.com>
2022-12-12 22:03:24 +0900

    refactoring:Items component 분리

---
+++ components/Items.tsx
@@ -0,0 +1,32 @@
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { IResult } from '../types/result'
+import styles from '../styles/items.module.css'
+
+interface Props {
+  results: Array<IResult>
+}
+export const Items = ({ results }: Props) => {
+  const router = useRouter()
+  const onClick = ({ id, title }: { id: number; title: string }) => {
+    router.push(`/movies/${title}/${id}`)
+  }
+  return (
+    <>
+      {results?.map((movie: IResult) => (
+        <div
+          onClick={() => onClick({ id: movie.id, title: movie.original_title })}
+          className={styles.movie}
+          key={movie.id}
+        >
+          <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
+          <h4>
+            <Link href={`/movies/${movie.original_title}/${movie.id}`}>
+              <a>{movie.original_title}</a>
+            </Link>
+          </h4>
+        </div>
+      ))}
+    </>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
-import Link from 'next/link'
-import { useRouter } from 'next/router'
+import axios from 'axios'
+import { Items } from '../components/Items'
 import Seo from '../components/Seo'
 import styles from '../styles/index.module.css'
 import { IResult } from '../types/result'
@@ -8,35 +8,18 @@ interface Props {
   results: Array<IResult>
 }
 export default function Home({ results }: Props) {
-  const router = useRouter()
-  const onClick = ({ id, title }: { id: number; title: string }) => {
-    router.push(`/movies/${title}/${id}`)
-  }
   return (
     <div className={styles.container}>
       <Seo title="Home" />
-      {results?.map((movie) => (
-        <div
-          onClick={() => onClick({ id: movie.id, title: movie.original_title })}
-          className={styles.movie}
-          key={movie.id}
-        >
-          <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
-          <h4>
-            <Link href={`/movies/${movie.original_title}/${movie.id}`}>
-              <a>{movie.original_title}</a>
-            </Link>
-          </h4>
-        </div>
-      ))}
+      <Items results={results}></Items>
     </div>
   )
 }
 
 export async function getServerSideProps() {
   const { results } = await (
-    await fetch(`http://localhost:3000/api/movies`)
-  ).json()
+    await axios.get(`http://localhost:3000/api/movies`)
+  ).data
   return {
     props: {
       results,
