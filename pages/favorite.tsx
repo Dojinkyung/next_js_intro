4031b8755060bbcba7106b5b2dd0f458329e280f
Dojinkyung <63532503+Dojinkyung@users.noreply.github.com>
2023-01-11 15:53:08 +0900

    feat:favorite 페이지

--- pages/favorite.tsx
+++ pages/favorite.tsx
@@ -1,10 +1,27 @@
 import Seo from '../components/Seo'
-
+import store from 'store'
+import styles from '../styles/favorite.module.css'
+import { Items } from '../components/Items'
+import { IResult } from '../types/result'
 export default function Favorite() {
+  const movies = store.get('fav') || []
   return (
     <div>
       <Seo title="favorite" />
-      <h1>Favorite</h1>
+      <h1 className={styles.big}>Favorite Movies({movies?.length})</h1>
+      <ul className={styles.favMovies}>
+        {movies.length > 0 ? (
+          movies.map((movie: IResult) => {
+            return (
+              <li key={movie.id}>
+                <Items movie={movie} />
+              </li>
+            )
+          })
+        ) : (
+          <div className={styles.noResults}>No Results</div>
+        )}
+      </ul>
     </div>
   )
 }
